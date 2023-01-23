634d0802b2fe407c736811a5c1821cfc893da9ff
Misael Valentin <misael.vf2@gmail.com>
2023-01-22 22:54:08 -0500

    Move BinaryRelations class into own module

--- pyrel/relations.py
+++ pyrel/relations.py
@@ -1,4 +1,3 @@
-import itertools
 from typing import Any, Callable, Iterator, Set
 
 
@@ -40,56 +39,3 @@ class BinaryRelation:
             return item in self._relation
         pairs = {pair for pair in self._relation}
         return item in pairs
-
-
-students = {
-    "Jason Goodfriend",
-    "Deborah Sherman",
-}
-
-courses = {"CS518", "CS510"}
-
-cartesian_product = itertools.product(students, courses)
-
-student_course = {
-    ("Jason Goodfriend", "CS518"),
-    ("Deborah Sherman", "CS518"),
-    ("Jason Goodfriend", "CS510"),
-}
-
-student_takes_course_relation = BinaryRelation(relation=student_course)
-
-
-city_state = {
-    ("Boulder", "Colorado"),
-    ("Bangor", "Maine"),
-    ("Ann Arbor", "Michigan"),
-    ("Middletown", "New Jersey"),
-    ("Cupertino", "California"),
-    ("Red Bank", "New Jersey"),
-}
-
-
-city_in_state_relation = BinaryRelation(relation=city_state)
-
-for pair in city_in_state_relation:
-    print(pair)
-
-print(("Boulder", "Colorado") in city_in_state_relation)
-
-# square_relation = BinaryRelation(
-#     relation=(range(10), map(lambda x: x**2, range(10)))
-# )
-
-
-def pairs(x, f):
-    for i in range(x):
-        yield (i, f(i))
-
-
-squares_relation = BinaryRelation(
-    from_function=True, domain={i for i in range(10)}, function=lambda x: x**2
-)
-
-for pair in squares_relation:
-    print(pair)

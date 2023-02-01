c8a319a9c2c254217f5f89c124f42367202ebdf2
Misael Valentin <misael.vf2@gmail.com>
2023-01-31 19:15:57 -0500

    Implement reflexive property

--- pyrel/relations.py
+++ pyrel/relations.py
@@ -9,13 +9,13 @@ class BinaryRelation:
         domain: Iterable[Any] | None = None,
         function: Callable[[Any], Any] | None = None,
     ) -> None:
-        self._relation = relation
 
+        self._relation = relation
         if from_function:
             if domain is None or function is None:
                 raise ValueError
-            self._domain = domain
             self._function = function
+        self._domain = domain
         self._from_function = from_function
 
     @property
@@ -23,7 +23,7 @@ class BinaryRelation:
         return self._relation
 
     def elements(self) -> tuple[Any, Any]:
-        if self._from_function:
+        if self._from_function and self._domain is not None:
             for x in self._domain:
                 yield (x, self._function(x))
         else:
@@ -31,7 +31,20 @@ class BinaryRelation:
                 for pair in self._relation:
                     yield pair
 
-    def __contains__(self, item) -> bool:
+    def is_reflexive(self):
+        if self._domain is not None and self._relation is not None:
+            for elem in self._domain:
+                if (elem, elem) not in self._relation:
+                    return False
+        return True
+
+    def is_symmetric(self):
+        return False
+
+    def is_transitive(self):
+        return False
+
+    def __contains__(self, item: tuple[Any, Any]) -> bool:
         if self._relation is None:
             return False
         return item in self._relation

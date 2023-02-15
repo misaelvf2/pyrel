ccae116f40b7345a14d45b50f06e6adb5c7cb6ac
Misael Valentin <misael.vf2@gmail.com>
2023-02-14 23:44:18 -0500

    Set domain and codomain for relations resulting from binary operations

--- pyrel/relations.py
+++ pyrel/relations.py
@@ -96,49 +96,114 @@ class BinaryRelation:
                         return False
         return True
 
-    def update(self, other_relation: Self) -> None:
-        self._domain |= other_relation.domain
-        self._codomain |= other_relation.codomain
-        self._relation |= other_relation.relation
+    def update(self, other_relation: Self) -> Self:
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        if isinstance(other_relation.domain, set):
+            self._domain.update(other_relation.domain)
+        if isinstance(other_relation.codomain, set):
+            self._codomain.update(other_relation.codomain)
+        self._relation.update(other_relation.relation)
+        return self
 
     def union(self, other_relation: Self) -> Self:
-        result_relation = self.relation
-        result_relation |= other_relation.relation
-        return self.__class__(relation=result_relation)
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        result_domain = self._merge_domains(other_relation)
+        result_codomain = self._merge_codomains(other_relation)
+        result_relation = self.relation.copy()
+        result_relation.update(other_relation.relation)
+        return self.__class__(
+            domain=result_domain, codomain=result_codomain, relation=result_relation
+        )
 
     def intersection(self, other_relation: Self) -> Self:
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        result_domain = self._merge_domains(other_relation)
+        result_codomain = self._merge_codomains(other_relation)
         result_relation = self.relation
         result_relation &= other_relation.relation
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=result_domain, codomain=result_codomain, relation=result_relation
+        )
 
     def difference(self, other_relation: Self) -> Self:
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        result_domain = self._merge_domains(other_relation)
+        result_codomain = self._merge_codomains(other_relation)
         result_relation = self.relation
         result_relation -= other_relation.relation
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=result_domain, codomain=result_codomain, relation=result_relation
+        )
 
     def symmetric_difference(self, other_relation: Self) -> Self:
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        result_domain = self._merge_domains(other_relation)
+        result_codomain = self._merge_codomains(other_relation)
         result_relation = self.relation
         result_relation ^= other_relation.relation
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=result_domain, codomain=result_codomain, relation=result_relation
+        )
 
     def compose(self, other_relation: Self) -> Self:
+        if self._unimplemented_operation(other_relation):
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
+        if self.codomain != other_relation.domain:
+            raise ValueError(
+                "First relation's codomain must be equal to the second relation's \
+                    domain"
+            )
         result_relation = set()
         for a, b in self.relation:
             for c, d in other_relation.relation:
                 if b == c:
                     result_relation.add((a, d))
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=self.domain.copy(),
+            codomain=other_relation.codomain.copy(),
+            relation=result_relation,
+        )
 
     def inverse(self) -> Self:
         result_relation = set()
         for a, b in self.relation:
             result_relation.add((b, a))
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=self.domain.copy(),
+            codomain=self.codomain.copy(),
+            relation=result_relation,
+        )
 
     def complement(self) -> Self:
-        result_relation = set(itertools.product(self._domain, self._codomain))
+        if isinstance(self.domain, set) and isinstance(self.codomain, set):
+            result_relation = set(itertools.product(self.domain, self.codomain))
+        else:
+            raise ValueError(
+                "Operation not implemented for class-based domains and codomains"
+            )
         result_relation -= self._relation
-        return self.__class__(relation=result_relation)
+        return self.__class__(
+            domain=self.domain.copy(),
+            codomain=self.codomain.copy(),
+            relation=result_relation,
+        )
 
     def isdisjoint(self, other_relation: Self) -> bool:
         return len(self.intersection(other_relation)) == 0
@@ -168,3 +233,23 @@ class BinaryRelation:
 
     def __len__(self) -> int:
         return len(self._relation)
+
+    def _unimplemented_operation(self, other_relation: Self | None = None) -> bool:
+        if isclass(self.domain) or isclass(self.codomain):
+            return True
+        if other_relation is not None:
+            if isclass(other_relation.domain) or isclass(other_relation.codomain):
+                return True
+        return False
+
+    def _merge_domains(self, other_relation) -> set:
+        result_domain = self.domain.copy()
+        if isinstance(other_relation.domain, set):
+            result_domain.update(other_relation.domain)
+        return result_domain
+
+    def _merge_codomains(self, other_relation) -> set:
+        result_codomain = self.codomain.copy()
+        if isinstance(other_relation.codomain, set):
+            result_codomain.update(other_relation.codomain)
+        return result_codomain
